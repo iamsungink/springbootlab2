ff21ad3e2238e899cb214ec003ee3e6a6bf9b8b7
iamsungink <iamsungink@naver.com>
2020-07-03 07:07:21 +0900

    .

--- src/ch02/community/src/test/java/com/demo/community/CommunityApplicationTests.java
+++ src/ch02/community/src/test/java/com/demo/community/CommunityApplicationTests.java
@@ -1,13 +1,48 @@
 package com.demo.community;
 
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 @SpringBootTest
 class CommunityApplicationTests {
 
+	@Value("${property.test.name}")
+	private String propertyTestName;
+
+	@Value("${propertyTest}")
+	private String propertyTest;
+
+	@Value("${noKey:default value}")
+	private String defaultValue;
+
+	@Value("${propertyTestList}")
+	private String[] propertyTestArray;
+
+	@Value("#{'${propertyTestList}'.split(',')}")
+	private List<String> propertyTestList;
+
 	@Test
-	void contextLoads() {
+	public void testValue(){
+		assertThat(propertyTestName, is("property depth test"));
+		assertThat(propertyTest, is("test"));
+		assertThat(defaultValue, is("default value"));
+		assertThat(propertyTestArray[0], is("a"));
 	}
+
+	@Test
+	@DisplayName("aaa")
+	public void getPlaces_NullpointerException_발생() {
+		System.out.println("test");
+	}
+
 
 }

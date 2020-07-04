6fe8a39c93f62000cafd95fa7e99dc4e77e76143
iamsungink <iamsungink@naver.com>
2020-07-05 07:47:02 +0900

    .

--- src/ch02/community/src/test/java/com/demo/community/pojo/FruitPropertyTest.java
+++ src/ch02/community/src/test/java/com/demo/community/pojo/FruitPropertyTest.java
@@ -1,5 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.demo.community.pojo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
 class FruitPropertyTest {
+
+    @Autowired
+    FruitProperty fruitProperty;
+
+    @Test
+    public void test(){
+
+        System.out.println(fruitProperty);
+
+        List<Map> fruitData = fruitProperty.getList();
+
+        assertThat(fruitData.get(0).get("name"), is("banana"));
+    }
 
 }

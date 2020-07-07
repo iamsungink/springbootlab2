ae6a8901cc1b5c0eb88283caffb28e988cc45128
iamsungink <iamsungink@naver.com>
2020-07-07 14:04:47 +0900

    ch04 commit

--- src/ch04/web/src/main/java/com/web/repository/UserRepository.java
+++ src/ch04/web/src/main/java/com/web/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.web.repository;
 
-public class UserRepository {
+import com.web.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by KimYJ on 2017-07-12.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
